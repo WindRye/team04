dfa04e7fe3fa4a8d6fb40c106bdfda1280c3c0c8
WindRye <huangtingfung20030801@gmail.com>
2023-10-25 11:56:29 +0800

    新增comment註解

--- database/migrations/2023_10_11_061427_create_pokemons_table.php
+++ database/migrations/2023_10_11_061427_create_pokemons_table.php
@@ -15,14 +15,14 @@ class CreatePokemonsTable extends Migration
     {
         Schema::create('pokemons', function (Blueprint $table) {
             $table->id();
-            $table->string('pokemon')->comment('寶可夢');
+            $table->string('pokemon')->comment('寶可夢(主鍵)');
             $table->string('region')->nullable()->comment('地區');
-            $table->foreignId('tid1')->nullable()->comment('屬性編號1');
+            $table->foreignId('tid1')->nullable()->comment('屬性編號1(外部鍵)');
             $table->foreign('tid1')
                     ->references('id')
                     ->on('types')
                     ->onDelete('cascade');
-            $table->integer('tid2')->comment('屬性編號2');
+            $table->integer('tid2')->comment('屬性編號2(外部鍵)');
             $table->foreign('tid2')
             ->references('id')
             ->on('types')
