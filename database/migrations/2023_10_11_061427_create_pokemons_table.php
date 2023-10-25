55fd36c28cff01225da8dc329d3e6a37c8b48669
WindRye <huangtingfung20030801@gmail.com>
2023-10-25 13:14:41 +0800

    修改外部鍵

--- database/migrations/2023_10_11_061427_create_pokemons_table.php
+++ database/migrations/2023_10_11_061427_create_pokemons_table.php
@@ -17,16 +17,16 @@ class CreatePokemonsTable extends Migration
             $table->id();
             $table->string('pokemon')->comment('寶可夢(主鍵)');
             $table->string('region')->nullable()->comment('地區');
-            $table->foreignId('tid1')->nullable()->comment('屬性編號1(外部鍵)');
+            $table->foreignId('tid1')->comment('屬性編號1(外部鍵)');
             $table->foreign('tid1')
                     ->references('id')
                     ->on('types')
                     ->onDelete('cascade');
             $table->integer('tid2')->comment('屬性編號2(外部鍵)');
             $table->foreign('tid2')
-            ->references('id')
-            ->on('types')
-            ->onDelete('cascade');
+                    ->references('id')
+                    ->on('types')
+                    ->onDelete('cascade');
             $table->double('height')->nullable()->unsigned()->comment('身高');
             $table->double('weight')->nullable()->unsigned()->comment('體重');
             $table->string('gender')->nullable()->comment('性別');
