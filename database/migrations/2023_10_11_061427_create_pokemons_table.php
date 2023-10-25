f6f48777e6e2ecb74b79e55acf6839399a553c62
WindRye <huangtingfung20030801@gmail.com>
2023-10-25 10:06:47 +0800

    新增欄位

--- database/migrations/2023_10_11_061427_create_pokemons_table.php
+++ database/migrations/2023_10_11_061427_create_pokemons_table.php
@@ -15,11 +15,14 @@ class CreatePokemonsTable extends Migration
     {
         Schema::create('pokemons', function (Blueprint $table) {
             $table->id();
-            $table->string('pokemon');
+            $table->string('pokemon')->nullable()->comment('寶可夢');
             $table->string('region');
-            $table->string('gender');
-            $table->string('ability');
-            $table->string('debug');
+            $table->integer('tid1')->unsigned()->nullable()->comment('屬性編號1');
+            $table->integer('tid2')->unsigned()->nullable()->comment('屬性編號2');
+            $table->double('height')->unsigned()->comment('身高');
+            $table->double('weight')->unsigned()->comment('體重');
+            $table->string('gender')->comment('性別');
+            $table->string('ability')->nullable()->comment('特性');
             $table->timestamps();
         });
     }
--- database/migrations/2023_10_11_061550_create_types_table.php
+++ database/migrations/2023_10_11_061550_create_types_table.php
@@ -15,20 +15,20 @@ class CreateTypesTable extends Migration
     {
         Schema::create('types', function (Blueprint $table) {
             $table->id();
-            $table->string('type');
-            $table->string('super_effective1');
-            $table->string('super_effective2');
-            $table->string('super_effective3');
-            $table->string('super_effective4');
-            $table->string('super_effective5');
-            $table->string('not_very_effective1');
-            $table->string('not_very_effective2');
-            $table->string('not_very_effective3');
-            $table->string('not_very_effective4');
-            $table->string('not_very_effective5');
-            $table->string('not_very_effective6');
-            $table->string('not_very_effective7');
-            $table->string('not_effective');
+            $table->string('type')->comment('屬性');
+            $table->string('super_effective1')->comment('效果絕佳1');
+            $table->string('super_effective2')->comment('效果絕佳2');
+            $table->string('super_effective3')->comment('效果絕佳3');
+            $table->string('super_effective4')->comment('效果絕佳4');
+            $table->string('super_effective5')->comment('效果絕佳5');
+            $table->string('not_very_effective1')->comment('收效甚微1');
+            $table->string('not_very_effective2')->comment('收效甚微2');
+            $table->string('not_very_effective3')->comment('收效甚微3');
+            $table->string('not_very_effective4')->comment('收效甚微4');
+            $table->string('not_very_effective5')->comment('收效甚微5');
+            $table->string('not_very_effective6')->comment('收效甚微6');
+            $table->string('not_very_effective7')->comment('收效甚微7');
+            $table->string('not_effective')->comment('技能無效');
             $table->timestamps();
         });
     }
