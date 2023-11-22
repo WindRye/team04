8456e0fef2a2a1bb5e01007b49f3d615831153e4
lemon <call911224>
2023-11-22 14:59:25 +0800

    新增資料

---
+++ resources/views/pokemons/index.blade.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+    <title>列出所有寶可夢</title>
+</head>
+<body>
+<h1>列出所有寶可夢</h1>
+
+@for($i=0; $i<count($pokemons);$i++)
+    {{ $pokemons[$i]['pokemon'] }}<br/>
+    {{ $pokemons[$i]['region'] }}<br/>
+    {{ $pokemons[$i]['height'] }}<br/>
+    {{ $pokemons[$i]['weight'] }}<br/>
+    {{ $pokemons[$i]['gender'] }}<br/>
+    {{ $pokemons[$i]['ability'] }}<br/><br/>
+
+@endfor
+
+
+</body>
+</html>
