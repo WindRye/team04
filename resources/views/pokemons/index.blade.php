f96fff95ad0600d2ce27cb3f7aee44ae3fca9c2d
D1104182102 <d1104182102@gm.lhu.edu.tw>
2023-11-24 08:47:02 +0800

    新增網頁檢視內容 找到無法migrate的原因:port預設3306 而DB那邊是3308

---
+++ resources/views/pokemons/index.blade.php
@@ -0,0 +1,22 @@
+<html>
+
+<html>
+<title>列出所有寶可夢</title>
+</head>
+
+<body>
+
+<h1>列出所有寶可夢</h1>
+
+@for($i=0; $i<count($pokemons); $i++)
+{{$pokemons[$i]['pokemon'] }}<br/>
+{{$pokemons[$i]['region'] }}<br/>
+{{$pokemons[$i]['gender'] }}<br/>
+{{$pokemons[$i]['ability'] }}<br/>
+{{$pokemons[$i]['height'] }}<br/>
+{{$pokemons[$i]['weight'] }}<br/><br/>
+@endfor
+
+</body>
+</html>
+
