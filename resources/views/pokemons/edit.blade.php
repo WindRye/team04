d5b0ed3351dbce47f18cbe68c724ae20771ae10c
林宗揚 <billy62724@gmail.com>
2023-12-27 10:32:29 +0800

    為pokemons.edit視圖增加資料驗證的錯誤訊息

--- resources/views/pokemons/edit.blade.php
+++ resources/views/pokemons/edit.blade.php
@@ -5,6 +5,7 @@
 @section('pokemon_theme', '編輯中的寶可夢')
 
 @section('pokemon_contents')
+    @include('message.list')
     {!! Form::model($pokemon, ['method' => 'PATCH','action' => ['\App\Http\Controllers\PokemonsController@update', $pokemon->id]]) !!}
     @include('pokemons.form', ['submitButtonText'=>"更新寶可夢資料"])
     {!! Form::close() !!}
